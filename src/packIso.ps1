96e332e7edbda7cf8371dce52850e2971e55e892
George Oastler <goastler4@gmail.com>
2025-09-12 12:16:46 +0100

    fixes

--- src/packIso.ps1
+++ src/packIso.ps1
@@ -152,8 +152,6 @@ function Install-Chocolatey {
     Write-ColorOutput "Installing Chocolatey package manager..." "Yellow"
     try {
         
-        Write-Log "Installing Chocolatey..."
-        # Install Chocolatey
         Set-ExecutionPolicy Bypass -Scope Process -Force
         [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
         iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
